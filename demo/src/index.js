87f64938d606086bf6264abba20dae140df9ff0b
jn.canches <jn.canches@e-deal.com>
2017-10-24 18:36:50 +0200

    git commit

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,27 @@
 import React, {Component} from 'react'
 import {render} from 'react-dom'
 
-import Example from '../../src'
+import Hand from '../../src'
 
 class Demo extends Component {
   render() {
     return <div>
       <h1>6nimmt-hand Demo</h1>
-      <Example/>
+      <Hand cards={this.getRandomCards()}/>
     </div>
   }
+
+    getRandomCards() {
+        var cards = [];
+        for (var i=0; i<10; i++) {
+            cards.push({faceValue: this.randomInt(1,104), zIndex: i});
+        }
+        return cards;
+    }
+
+    randomInt(min,max) {
+        return Math.floor(Math.random()*(max-min+1)+min);
+    }
 }
 
 render(<Demo/>, document.querySelector('#demo'))
--- src/index.js
+++ src/index.js
@@ -8,37 +8,40 @@ import Card from '6nimmt-card'
 export default class extends Component {
     constructor(props) {
         super(props);
-
-        var cards = this.getRandomCards();
-
-        this.state = {
-            cards: cards
-        };
+        this.sortCards = this.sortCards.bind(this);
     }
 
   render() {
       let handClass = classnames('hand');
 
-      const cardList = this.state.cards.map((card, i) =>
-          <div className="card-container" style={{'--card-z-index':card.zIndex}} key={i}>
-            <Card faceValue={card.faceValue}></Card>
-          </div>
+      const cardList = [].concat(this.props.cards)
+          .sort((a, b) => a.faceValue > b.faceValue)
+          .map((card, i) =>
+              <div className="card-container" style={{'--card-z-index':card.zIndex}} key={i}>
+                <Card faceValue={card.faceValue}></Card>
+              </div>
       );
 
     return <div className={handClass}>
         {cardList}
+        <div className="actions">
+            <button onClick={this.sortCards} value='ltr'>Sort -&gt;</button>
+            <br/>
+            <button onClick={this.sortCards} value='ltr'>Sort &lt;-</button>
+        </div>
     </div>
   }
 
-  getRandomCards() {
-      var cards = [];
-      for (var i=0; i<10; i++) {
-        cards.push({faceValue: this.randomInt(1,104), zIndex: i});
-      }
-      return cards;
+  sortCards(a,b,c) {
+        console.log(a,b,c);
+    this.setState({cards: this.state.cards.sort(this.cardsComparator)});
   }
 
-  randomInt(min,max) {
-    return Math.floor(Math.random()*(max-min+1)+min);
-  }
+  cardsComparator(a, b) {
+      if (a.faceValue < b.faceValue)
+        return -1;
+      if (a.faceValue > b.faceValue)
+          return 1;
+      return 0;
+    }
 }
